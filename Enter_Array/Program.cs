a6b7e1759782baa45bf9e49946ff7e129c873371
Admin <Admin@DPASUTP-17535>
2021-04-01 17:11:07 +0700

    Добавьте файлы проекта.

---
+++ Enter_Array/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text;
+
+namespace Enter_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Find2Max(Enter_valuesArray(Enter_sizeArray()));
+        }
+
+        static int Enter_sizeArray ()
+        {
+            int size  = default;
+            bool a = true;
+            while (a)
+            {
+                try
+                {
+                    Console.Write("\n" + "    Please, enter size of array: ");
+                    size = Convert.ToInt32(Console.ReadLine());
+                    if (size < 2)
+                    {
+                        Console.WriteLine("\n" + "   ENTER ERROR! Please, try again.");
+                    }
+                    else
+                    {
+                        a = false;
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("\n" + "   ENTER ERROR! Please, try again.");
+
+
+                }
+            }
+            return size;
+        }
+        
+        static int[] Enter_valuesArray(in int size)
+        {
+            int[] massive = new int[size];
+            int num = 0;
+            while (num < size)
+            {
+               
+                    try
+                    {
+                        Console.Write("\n" + $"    Please, enter value of array's element with index {num}: ");
+                        massive[num] = Convert.ToInt32(Console.ReadLine());
+                        num++;
+                        
+                    }
+                    catch
+                    {
+                        Console.WriteLine("\n" + "   ENTER ERROR! Please, try again.");
+                        
+                    }
+                
+                
+            }
+            var strA = new StringBuilder();
+            for (int i = 0; i <size; i++)
+            {
+                strA.Append($"{massive[i]} ");
+            }
+            Console.WriteLine("\n" + $"   Your array: ( {strA})");
+
+            return massive;
+        }
+        static void Find2Max(int[] massive)
+        {
+            int max, second_max;
+            if (massive[0] > massive[1])
+            { 
+                max = massive[0]; second_max = massive[1];
+            }
+            else
+            {
+                second_max = massive[0]; max = massive[1];
+            }
+
+            for (int i = 2; i < massive.Length; i++)
+            {
+                if (massive[i] >= max)
+                {
+                    second_max = max;
+                    max = massive[i];
+                }
+                else if (massive[i] > second_max)
+                {
+                    second_max = massive[i];
+                }
+            }
+            Console.WriteLine("\n" + $"Second max number in array: {second_max}");
+        }
+                            
+    }
+
+}
---
+++ Handicraft Calculator/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Handicraft_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            main_calc_menu();
+        }
+
+        static void main_calc_menu ()
+        {
+            int operand1, operand2, result, choice;
+            Console.WriteLine("Welcom to Handicraft Calculator!!! ");
+        link1:
+            try
+            {
+                Console.Write("\n"+"    Please, enter first number: ");
+                operand1 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("\n"+"    ERROR. INCORRECT VALUE OF FIRST NUMBER!");
+                goto link1;
+            }
+        link2:
+            try
+            {
+                Console.Write("\n"+"    Please, enter second number: ");
+                operand2 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("\n"+"    ERROR. INCORRECT VALUE OF SECOND NUMBER!");
+                goto link2;
+            }
+        link3:
+            try
+            {
+                Console.WriteLine("\n" + "    1 - Addition" + "\n" + "    2 - Subtraction" + "\n" + "    3 - Multiplication" + "\n" + "    4 - Division (Print the remainder)" + "\n" + "    5 - Division (Print the integer part)"+ "\n"+"\n"  + "    6 - Return on enter first operand"+ "\n" + "    7 - Return on enter second operand" + "\n"+ "    8 - Close Handicraft Calculator");
+                Console.Write("\n" + "    Please, select an operation from the list and enter the relevant number: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice>8 | choice<1) { Console.WriteLine("\n" + "    ERROR. INCORRECT NUMBER OF CHOICE!"); goto link3; }
+                if (choice == 1) 
+                {
+                    try { result = operand1 + operand2; } catch { Console.WriteLine("\n" + "    ERROR. CAN'T CALCULATE!"); goto link3; }
+                    Console.WriteLine("\n" + $"     {operand1} + {operand2} = {operand1+operand2} ");
+                    Console.WriteLine("\n" + "     Press any key to make a new calculation..");
+                    Console.ReadKey();
+                    goto link1;
+                }
+                else if (choice == 2)
+                {
+                    try { result = operand1 - operand2; } catch { Console.WriteLine("\n" + "    ERROR. CAN'T CALCULATE!"); goto link3; }
+                    Console.WriteLine("\n" + $"     {operand1} - {operand2} = {operand1 - operand2} ");
+                    Console.WriteLine("\n" + "    Press any key to make a new calculation..");
+                    Console.ReadKey();
+                    goto link1;
+                }
+                else if (choice == 3)
+                {
+                    try { result = operand1 * operand2; } catch { Console.WriteLine("\n" + "    ERROR. CAN'T CALCULATE!"); goto link3; }
+                    Console.WriteLine("\n" + $"     {operand1} * {operand2} = {operand1 * operand2} ");
+                    Console.WriteLine("\n" + "    Press any key to make a new calculation..");
+                    Console.ReadKey();
+                    goto link1;
+                }
+                else if (choice == 4)
+                {
+                    try { result = operand1 % operand2; } catch { Console.WriteLine("\n" + "    ERROR. CAN'T CALCULATE!"); goto link3; }
+                    Console.WriteLine("\n" + $"     {operand1} % {operand2} = {operand1 % operand2} ");
+                    Console.WriteLine("\n" + "    Press any key to make a new calculation..");
+                    Console.ReadKey();
+                    goto link1;
+                }
+                else if (choice == 5)
+                {
+                    try { result = operand1 / operand2; } catch { Console.WriteLine("\n" + "    ERROR. CAN'T CALCULATE!"); goto link3; }
+                    Console.WriteLine("\n" + $"     {operand1} / {operand2} = {operand1 / operand2} ");
+                    Console.WriteLine("\n" + "    Press any key to make a new calculation..");
+                    Console.ReadKey();
+                    goto link1;
+                }
+                else if (choice == 6)
+                {
+                    goto link1;
+                }
+                else if (choice == 7)
+                {
+                    goto link2;
+                }
+                else if (choice == 8)
+                {        
+                    Environment.Exit(0);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("\n" + "    ERROR Incorrect number of choice!");
+                goto link3;
+            }
+        
+            
+        }
+    }
+}
